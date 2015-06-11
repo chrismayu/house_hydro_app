b460ca0c2f72808e8f13fac4f44005a772d8062c
chrismayu <chrismayu@gmail.com>
2015-06-11 10:07:02 -0400

    added data for home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+  
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+   
+   before_filter :find_lastest
+  
+  private
+  
+  def find_lastest
+    @lastest_data = InComing.latest_data
+    @all_data = InComing.current
+    #@lastest_feed = Watcher.where("feedmode_active = ?", true).first
+    #@lastest_waterchange = Watcher.where("waterchange_active= ?", true).first
+    #@reef_tank = ReefTank.first
+  end
+  
+  
 end
--- app/models/in_coming.rb
+++ app/models/in_coming.rb
@@ -7,7 +7,19 @@ class InComing < ActiveRecord::Base
  
  after_create :update_stat
  
+ def self.current(hidden_ids = nil)
+   result = where("created_at >= ?", 5.week.ago.utc).order("created_at DESC")
+  # result = result.where("starts_at <= :now and ends_at >= :now", now: Time.zone.now)
+  # result = result.where("id not in (?)", hidden_ids) if hidden_ids.present?
+   result
+ end
  
+ def self.latest_data
+   result = order("created_at DESC").first
+  # result = result.where("starts_at <= :now and ends_at >= :now", now: Time.zone.now)
+  # result = result.where("id not in (?)", hidden_ids) if hidden_ids.present?
+   result
+ end
  
 
  private
